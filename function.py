02535fa2bff26844e5b413d9a0ab0be09a5c6398
ksg <kaeh3403@gmail.com>
2024-11-18 03:13:44 +0000

    🔥 Remove: label 별 wandb log 삭제

--- function.py
+++ function.py
@@ -72,8 +72,7 @@ def validation(epoch, model, CLASSES, data_loader, criterion, model_type, thr=0.
         f"{c:<12}: {d.item():.4f}"
         for c, d in zip(CLASSES, dices_per_class)
     ]
-    for c, d in zip(CLASSES, dices_per_class):
-        wandb.log({f'{c}_dice': d})
+
     dice_str = "\n".join(dice_str)
     print(dice_str)
     
