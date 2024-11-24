0bdd64aff1d28cf5014c469f35f34827890b129f
SuyoungPark11 <suyoung.park1121@gmail.com>
2024-11-24 18:05:38 +0900

    ✨ Feature: by_epochs

--- mmenv/config/config.py
+++ mmenv/config/config.py
@@ -9,9 +9,10 @@ custom_imports = dict(
     imports=[
         'utils.mixin',
         'utils.dataset',
-        'utils.metric'
+        'utils.metric',
+        'mmseg.models.data_preprocessor'
     ],  
-    allow_failed_imports=False 
+    allow_failed_imports=True
 )
 
 data_preprocessor = dict(
@@ -46,7 +47,7 @@ model = dict(
 )
 
 # optimizer
-optimizer = dict(type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01)
+optimizer = dict(type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.01)
 optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
 
 # mixed precision
@@ -55,26 +56,30 @@ fp16 = dict(loss_scale='dynamic')
 # learning policy
 param_scheduler = [
     dict(
-        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500
+        type='LinearLR', 
+        start_factor=1e-6, 
+        by_epoch=True, 
+        begin=0, 
+        end=5
     ),
     dict(
         type='PolyLR',
         eta_min=0.0,
         power=1.0,
-        begin=1500,
-        end=20000,
-        by_epoch=False,
+        begin=5,
+        end=100,
+        by_epoch=True,
     )
 ]
 # training schedule for 20k
-train_cfg = dict(type='IterBasedTrainLoop', max_iters=20000, val_interval=2000)
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=100, val_interval=5)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 default_hooks = dict(
     timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    logger=dict(type='LoggerHook', interval=1, log_metric_by_epoch=True),
     param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=2000),
+    checkpoint=dict(type='CheckpointHook', by_epoch=True, interval=10),
     sampler_seed=dict(type='DistSamplerSeedHook'),
     visualization=dict(type='SegVisualizationHook')
 )
--- mmenv/run.py
+++ mmenv/run.py
@@ -1,14 +1,60 @@
+import os
+import cv2
+import argparse
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn.functional as F
+from tqdm.auto import tqdm
+from collections import defaultdict
+from mmseg.registry import MODELS
 from mmengine.config import Config
-from mmengine.runner import Runner
+from mmengine.runner import Runner, load_checkpoint
+from mmengine.dataset import Compose
+
+import test
 
 
-cfg = Config.fromfile("./config/config.py")
-cfg.launcher = "none"
-cfg.work_dir = "checkpoints"
+CLASSES = [
+    'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
+    'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
+    'finger-11', 'finger-12', 'finger-13', 'finger-14', 'finger-15',
+    'finger-16', 'finger-17', 'finger-18', 'finger-19', 'Trapezium',
+    'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
+    'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
+]
 
-# resume training
-cfg.resume = False
-runner = Runner.from_cfg(cfg)
+CLASS2IND = {v: i for i, v in enumerate(CLASSES)}
+IND2CLASS = {v: k for k, v in CLASS2IND.items()}
 
-# start training
-runner.train()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mode', type=str, required=True, help="Select train or test mode")
+    parser.add_argument('-c', '--config', type=str, required=True, help="Path to the configuration YAML file")
+    args = parser.parse_args()
+
+    if not args.mode in ['train', 'test']:
+        raise Exception("Select train or test mode")
+    
+    if not os.path.exists(args.config):
+        raise FileNotFoundError(f"Config file {args.config} does not exist.")
+    
+    cfg = Config.fromfile(args.config)
+    cfg.launcher = "none"
+    cfg.work_dir = "checkpoints"
+
+    if args.mode == 'train':
+        cfg.resume = False
+        runner = Runner.from_cfg(cfg)
+        runner.train()
+
+    elif args.mode == 'test':
+        model = MODELS.build(cfg.model)
+        checkpoint = load_checkpoint(
+            model,
+            "/data/ephemeral/home/syp/level2-cv-semanticsegmentation-cv-20-lv3/mmenv/checkpoints/iter_20000.pth",
+            map_location='cpu'
+        )
+        df_inference = test.main(cfg, model)
+        df_inference.to_csv('submission.csv', index=False)
